e12787dd9f0075ce5354f03952192a6eb883ba96
Greg Wylie <grug_life@Gregs-MacBook-Pro.local>
2025-11-17 21:36:45 -0800

    add future  value function

--- tvm.py
+++ tvm.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import numpy as np
-
 def present_value(rate, n_periods, payment=0, future_value=0, when=0):
     """
     Computes the present value of an annuity or lump sum
@@ -28,6 +25,7 @@ def present_value(rate, n_periods, payment=0, future_value=0, when=0):
     present_value : float
         Present value of investment
     """
+
     if rate == 0:
         return -(payment * n_periods + future_value)
     
@@ -35,5 +33,38 @@ def present_value(rate, n_periods, payment=0, future_value=0, when=0):
     present_value = -(payment * (1 + rate * when) * pv_factor + future_value * (1+ rate) ** (-n_periods))
     return round(present_value, 2)
 
-print(present_value(rate=0.08, n_periods=5, future_value=1000))
-print(present_value(rate=0.06, n_periods=10, payment=100, when=1))
+
+def future_value(rate, n_periods, payment=0, present_value=0, when=0):
+    """ 
+    Computes the future value of an annuity or lump sum
+
+    Parameters
+    ----------
+    rate : float
+        Discount rate per period (e.g., 0.05 for 5%)
+
+    n_periods : int
+        Number of periods
+
+    payment : float
+        Payment made each period (PMT)
+
+    present_value : float
+        Present lump sum (PV)
+
+    when : {0, 1}
+        0 = end of period (ordinary annuity)
+        1 = beginning of period (annuity due)
+
+    Returns
+    -------
+    future_value : float
+        Future value of investment
+    """
+
+    if rate == 0:
+        return present_value + n_periods * payment
+    
+    fv_factor = ((1 + rate) ** n_periods -1) / rate
+    future_value = (-(present_value * (1 + rate) ** n_periods) - payment * (1 + rate * when) * fv_factor)
+    return round(future_value, 2)
